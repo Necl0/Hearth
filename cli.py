96a42db3b1ce7d15a616441f89279e7d9705756e
Josiah <63004866+Necl0@users.noreply.github.com>
2022-12-05 17:09:02 -0500

    Added new commands

--- cli.py
+++ cli.py
@@ -3,6 +3,9 @@ from rich import print
 import json
 import datetime
 import webbrowser
+import random
+import string
+import os
 
 app = typer.Typer()
 bm = typer.Typer()
@@ -11,11 +14,15 @@ app.add_typer(bm, name="bm")
 
 @app.command()
 def init():
+    os.system("cls" if os.name == "nt" else "clear")
+
     print("\nWelcome to [bold blue]Nota[/bold blue], the CLI bm tool! Type 'exit' to quit.\n")
 
 
 @app.command()
 def exit():
+    os.system("cls" if os.name == "nt" else "clear")
+
     print('\n[bold red]Exiting...[/bold red]')
     raise typer.Exit()
 
@@ -27,6 +34,7 @@ def add(
         tag: str = typer.Argument(..., help="Tag for the bookmark")
         ):
     """Add a bookmark to the cli to be used later"""
+    os.system("cls" if os.name == "nt" else "clear")
 
     if name == "":
         print("Invalid name. Please enter a name.")
@@ -36,17 +44,18 @@ def add(
         banned = f.read().splitlines()
         for word in banned:
             if word in name:
-                print(f"Error: query contains banned language. Please remove {word} from your query.")
+                print(f"\n[red]Error [/red]: query contains banned language. Please remove {word} from your query.\n")
                 return
 
     with open("bookmarks.json", "r") as f:
         bms = json.load(f)
 
         if name in bms:
-            print(f"Error: bookmark {name} already exists. Please choose a different name.")
+            print(f"\n[red] Error [/red]: bookmark {name} already exists. Please choose a different name.\n")
             return
 
         bms[name] = {
+            "id": "".join(random.choices(string.ascii_lowercase + string.digits, k=10)),
             "name": name,
             "url": url,
             "tag": tag,
@@ -55,12 +64,14 @@ def add(
 
         with open("bookmarks.json", "w") as f:
             json.dump(bms, f, indent=4)
-            print(f"Added bookmark [blue] {name} [/blue]to bookmarks.json")
+            print(f"\nAdded bookmark [blue] {name} [/blue]to bookmarks.json\n")
 
 
 @bm.command()
 def list():
     """List all bms"""
+    os.system("cls" if os.name == "nt" else "clear")
+
     with open("bookmarks.json", "r") as f:
         bms = json.load(f)
 
@@ -69,36 +80,46 @@ def list():
         for bm in bms:
             print(f"[bold blue]{bm}[/bold blue]")
             for key, value in bms[bm].items():
-                print(f"{key}: {value}")
+                if key!="name":
+                    print(f"{key}: {value}")
             print()
+
 
 @bm.command()
 def delete(name: str = typer.Argument(..., help="Name of the bm to delete")):
     """Delete a bm from bookmarks.json"""
+    os.system("cls" if os.name == "nt" else "clear")
+
     with open("bookmarks.json", "r") as f:
         bms = json.load(f)
         if name in bms:
             del bms[name]
             with open("bookmarks.json", "w") as f:
                 json.dump(bms, f, indent=4)
-                print(f"Deleted bm[blue] {name} [/blue]from bookmarks.json")
+                print(f"\nDeleted bookmark[blue] {name} [/blue]from bookmarks.json\n")
         else:
-            print(f"Error: bookmark {name} does not exist.")
+            print(f" \n[red]Error[/red]: bookmark {name} does not exist.\n")
+
 
 @bm.command()
 def view(name: str = typer.Argument(..., help="Name of the bm to open")):
     """Open a bookmark in the browser"""
+
+    os.system("cls" if os.name == "nt" else "clear")
+
     with open("bookmarks.json", "r") as f:
         bms = json.load(f)
         if name in bms:
             webbrowser.open(bms[name]["url"])
         else:
-            print(f"Error: bookmark {name} does not exist.")
+            print(f" \n[red]Error[/red]: bookmark {name} does not exist.\n")
 
 
 @bm.command()
 def search(query: str = typer.Argument(..., help="Query to search for")):
     """Search for a bookmark"""
+
+    os.system("cls" if os.name == "nt" else "clear")
 
     with open("bookmarks.json", "r") as f:
         bms = json.load(f)
@@ -111,11 +132,14 @@ def search(query: str = typer.Argument(..., help="Query to search for")):
                 print()
                 return
 
-    print(f"\nNo bookmark found for query [bold blue]{query} [/bold blue]\n")
+    print(f"\n No bookmark found for query [bold blue]{query} [/bold blue]\n")
+
 
 @bm.command()
 def update(name: str = typer.Argument(..., help="Name of the bm to update")):
     """Update a bookmark"""
+
+    os.system("cls" if os.name == "nt" else "clear")
 
     # update a bm
     with open("bookmarks.json", "r") as f:
@@ -128,22 +152,27 @@ def update(name: str = typer.Argument(..., help="Name of the bm to update")):
             bms[name]["tag"] = tag
             with open("bookmarks.json", "w") as f:
                 json.dump(bms, f, indent=4)
-                print(f"Updated bookmark [blue] {name} [/blue]to bookmarks.json")
+                print(f"\nUpdated bookmark [blue] {name} [/blue]to bookmarks.json\n")
         else:
-            print(f"Error: bookmark {name} does not exist.")
+            print(f" \n[red]Error[/red]: bookmark {name} does not exist.\n")
+
 
 @bm.command()
 def clear():
     """Clear all bookmarks"""
 
-    # check if you want to proceed
-    print("Are you sure you want to clear all bookmarks? [bold red]This cannot be undone.[/bold red]")
-    print("Type [bold blue]yes[/bold blue] to proceed.")
+    os.system("cls" if os.name == "nt" else "clear")
 
-    if input().lower().strip() == "yes":
+    # check if you want to proceed
+    print("""\nAre you sure you want to clear all bookmarks? [bold red]This cannot be undone.[/bold red]
+    Type [bold blue]yes[/bold blue] to proceed or [bold blue]no[/bold blue] to cancel.\n""")
+
+
+    if input(">>> ").lower().strip() == "yes":
         with open("bookmarks.json", "w") as f:
             json.dump({}, f, indent=4)
             print("Cleared bookmarks")
+
 
 @bm.command()
 def count():
