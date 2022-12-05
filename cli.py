907ee136d94d0a9f2fef278fd39f0f2f3dd01bca
Josiah <63004866+Necl0@users.noreply.github.com>
2022-12-04 22:49:01 -0500

    Update cli.py

--- cli.py
+++ cli.py
@@ -7,15 +7,18 @@ openai.api_key = "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"
 
 app = typer.Typer()
 
-def main(engine: str,
-         query: str,
+
+def main(
+         engine: str = typer.Argument('...', help='The engine to use for completion.'),
+         query: str = typer.Argument('...', help='The prompt to use for completion.'),
          temp: float = typer.Argument(0.5, help="The higher the temperature, the more random the completions. Try 0.9 for more adventurous completions."),
          max_t: int = typer.Argument(2048, help="The maximum number of tokens to generate. The default is 2048 for curie, babbage, and ada. 4000 for davinci."),
-         top_p: float = typer.Argument(1),
-         freq_pen :float = typer.Argument(0),
-         pre_pen: float = typer.Argument(0)) -> None:
+         top_p: float = typer.Argument(1, help="engine considers the results of the tokens with top_p probability mass. "),
+         freq_pen: float = typer.Argument(0, help="engine prefers words that were not used recently."),
+         pre_pen: float = typer.Argument(0, help="engine prefers completions that have a certain subsequence present."),
+        ) -> None:
 
-    """Use OpenAI's ChatGPT engine to generate text.
+    """Use OpenAI's engines to generate text.
 
     Args:
     - engine (str): The engine to use. Can be davinci, curie, babbage, or ada.
@@ -28,8 +31,19 @@ def main(engine: str,
 
     """
 
+    if query == "":
+        print("Invalid query. Please enter a query.")
+        return
+
+    with open("banned.txt", "r") as f:
+        banned = f.read().splitlines()
+        for word in banned:
+            if word in query:
+                print(f"Error: query contains banned language. Please remove {word} from your query.")
+                return
 
     assert engine in ["davinci", "curie", "babbage", "ada"], "Invalid engine. Must be davinci, curie, babbage, or ada."
+
     assert 0 <= temp <= 1, "Temperature must be between 0 and 1."
     assert 0 <= top_p <= 1, "Top_p must be between 0 and 1."
     assert 0 <= freq_pen <= 1, "Frequency penalty must be between 0 and 1."
@@ -37,26 +51,12 @@ def main(engine: str,
 
     match engine:
         case "davinci":
-            assert max_t <= 4000, "Maximum tokens must be less than or equal to 4000."
-        case "curie":
-            assert max_t <= 2048, "Maximum tokens must be less than or equal to 2048."
-            engine_id = "curie-instruct-beta"
-        case "babbage":
-            assert max_t <= 2048, "Maximum tokens must be less than or equal to 2048."
-            engine_id = "babbage"
-        case "ada":
-            assert max_t <= 2048, "Maximum tokens must be less than or equal to 2048."
-            engine_id = "ada"
-
-
-    # validate query is correct
-    if query == "":
-        print("Invalid query. Please enter a query.")
-        return
-
+            assert max_t <= 4000, "Maximum tokens must be <= 4000."
+        case "curie" | "babbage" | "ada":
+            assert max_t <= 2048, "Maximum tokens must be <= 2048."
 
     response = openai.Completion.create(
-        engine=engine_id,
+        engine=engine,
         prompt=query,
         temperature=temp,
         max_tokens=max_t,
@@ -66,13 +66,11 @@ def main(engine: str,
         stop=["\n", " Human:", " AI:"]
     )
 
-    print("Query:", query)
-    if (query.lower().strip() == 'exit'):
+    if query.lower().strip() == 'exit':
         print('\n[bold red]Exiting...[/bold red]')
         exit()
 
     print(response["choices"][0]["text"])
-
 
 
 if __name__ == "__main__":
